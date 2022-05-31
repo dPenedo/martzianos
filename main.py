3afc931c14f1b3dca175ce48d73c01cd6c4acf76
dPenedo <dpenedo@tutanota.com>
2022-05-31 17:51:55 -0300

    Some little changes on players and bullets movements

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ player_x = 368
 player_y = 500
 player_x_change = 0
 player_y_change = 0
+player_speed = 2
 # Puntaje
 
 
@@ -39,6 +40,7 @@ enemy_x = []
 enemy_y =[]
 enemy_x_change = []
 enemy_y_change = []
+enemy_speed = 2
 number_enemies = 8
 
 
@@ -46,14 +48,14 @@ for e in range(number_enemies):
     img_enemy.append(pygame.image.load("img/ufo.png"))
     enemy_x.append(random.randint(0, 736))
     enemy_y.append(random.randint(50, 180))
-    enemy_x_change.append(0.6)
+    enemy_x_change.append(enemy_speed)
     enemy_y_change.append(70)
 #  Variables of the bullet
 img_bala = pygame.image.load("img/bullet.png")
 bullet_x = 0
 bullet_y = 500
 bala_x_cambio = 0
-bala_y_cambio = 3
+bullet_speed = 5
 bullet_visible = False
 
 # Text of Gamve Over
@@ -105,12 +107,16 @@ def colision(x_1, y_1, x_2, y_2):
 
 music_on()
 
+FPS=60
+clock = pygame.time.Clock()
 #Loop of the game
 game_runing = True
 defeat = False
 while game_runing:
+    clock.tick(FPS)
     # Background image
     screen.blit(background, (0, 0))
+    player(player_x, player_y)
 
     # Iterate events
     for event in pygame.event.get():
@@ -118,16 +124,17 @@ while game_runing:
         if event.type == pygame.QUIT:
             game_runing = False
         # Event for pressing keys
+        keys=pygame.key.get_pressed()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player_x_change = -1
-            if event.key == pygame.K_RIGHT:
-                player_x_change = 1
-            if event.key == pygame.K_UP:
-                player_y_change = -1
-            if event.key == pygame.K_DOWN:
-                player_y_change = 1
-            if event.key == pygame.K_SPACE and bullet_visible == False and defeat == False:
+            if keys[pygame.K_LEFT]:
+                player_x_change -= player_speed
+            if keys[pygame.K_RIGHT]:
+                player_x_change += player_speed
+            if keys[pygame.K_UP]:
+                player_y_change -= player_speed
+            if keys[pygame.K_DOWN]:
+                player_y_change += player_speed
+            if keys[pygame.K_SPACE] and bullet_visible == False and defeat == False:
                 sonido_bala = mixer.Sound('audio/disparo.mp3')
                 sonido_bala.play()
                 bullet_x = player_x
@@ -142,6 +149,10 @@ while game_runing:
 
         # Event of releasing keys
         if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_x_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                player_y_change = 0
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player_x_change = 0
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
@@ -169,10 +180,10 @@ while game_runing:
 
         # Keep the enemies on the screen's borders
         if enemy_x[e]<= 0:
-            enemy_x_change[e]= 1
+            enemy_x_change[e] += enemy_speed
             enemy_y[e] += enemy_y_change[e]
         elif enemy_x[e] >= 736:
-            enemy_x_change[e] = -1
+            enemy_x_change[e] -= enemy_speed
             enemy_y[e] += enemy_y_change[e]
 
     #Colision
@@ -186,9 +197,9 @@ while game_runing:
             enemy_x[e]  = random.randint(0, 736)
             enemy_y[e]  = random.randint(50, 200)
         enemy(enemy_x[e], enemy_y[e], e)
+        
         player_y_area = player_y - 30
         final_colision = colision(enemy_x[e], enemy_y[e], player_x, player_y_area)
-        
         if final_colision:
             final_sound = mixer.Sound('audio/explosion_final.mp3')
             final_sound.play(0)
@@ -198,15 +209,14 @@ while game_runing:
             defeat = True
 
     # Bullet's location
-    if bullet_y <= -32:
-        bullet_y = 500
+    if bullet_y <= -10:
+        bullet_y = player_y
         bullet_visible = False
     if bullet_visible:
         shooting_bullet(bullet_x, bullet_y)
-        bullet_y -= bala_y_cambio
+        bullet_y -= bullet_speed
         
 
-    player(player_x, player_y)
     # Defeat
     if defeat == True:
         final_text()
